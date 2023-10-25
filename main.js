d3c2f56229c5ccbab3ad2d54f5c4407ecd219535
sctlcd <lucinda.souchet@gmail.com>
2023-10-25 22:08:14 +0200

    rendered cube

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 import './assets/style/style.css'
 
+const canvas = document.getElementById("render-canvas");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <h1>Hello Babylon.js!</h1>
-  </div>
-`
+const engine = new BABYLON.Engine(canvas);
+const scene = new BABYLON.Scene(engine);
+scene.clearColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+
+const camera = new BABYLON.FreeCamera(
+  "camera",
+  new BABYLON.Vector3(0, 0, -10),
+  scene,
+);
+
+const light = new BABYLON.PointLight(
+  "light",
+  new BABYLON.Vector3(10, 10, 0),
+  scene,
+);
+
+const box = BABYLON.Mesh.CreateBox("box", 2, scene);
+box.rotation.x = -0.2;
+box.rotation.y = -0.4;
+
+const boxMaterial = new BABYLON.StandardMaterial("material", scene);
+boxMaterial.emissiveColor = new BABYLON.Color3(0, 0.1, 0.86);
+box.material = boxMaterial;
+
+function renderLoop() {
+  scene.render();
+}
+engine.runRenderLoop(renderLoop);
