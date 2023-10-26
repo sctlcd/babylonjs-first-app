57ecfd29fc8ee66a5b266c838edb55d106410d7d
sctlcd <lucinda.souchet@gmail.com>
2023-10-26 12:04:21 +0200

    add more geometries and animations

--- main.js
+++ main.js
@@ -29,11 +29,35 @@ box.rotation.x = -0.2;
 box.rotation.y = -0.4;
 
 const boxMaterial = new BABYLON.StandardMaterial("material", scene);
-boxMaterial.emissiveColor = new BABYLON.Color3(1, 0, 0.1);
+boxMaterial.emissiveColor = new BABYLON.Color3.FromHexString('#a61308');
 box.material = boxMaterial;
+
+// Creating the torus
+const torus = BABYLON.Mesh.CreateTorus("torus", 2, 0.5, 15, scene);
+torus.position.x = -5;
+torus.rotation.x = 1.5;
+
+const torusMaterial = new BABYLON.StandardMaterial("material", scene);
+torusMaterial.emissiveColor = new BABYLON.Color3.FromHexString('#2400ab');
+torus.material = torusMaterial;
+
+// Creating the cylinder
+const cylinder = BABYLON.Mesh.CreateCylinder("cylinder", 2, 2, 2, 12, 1, scene);
+cylinder.position.x = 5;
+cylinder.rotation.x = -0.2;
+
+const cylinderMaterial = new BABYLON.StandardMaterial("material", scene);
+cylinderMaterial.emissiveColor = new BABYLON.Color3.FromHexString('#e6d307');
+cylinder.material = cylinderMaterial;
+
+let t = 0;
 
 // Creating a rendering loop to make the scene visible
 function renderLoop() {
   scene.render();
+  t -= 0.01;
+  box.rotation.y = t*3;
+  torus.scaling.z = Math.abs(Math.sin(t*2.5))+0.8;
+  cylinder.position.y = Math.sin(t*5);
 }
 engine.runRenderLoop(renderLoop);
